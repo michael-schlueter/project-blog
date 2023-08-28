31b7501d2824399d03da3cf7241b4ded5fbff003
Michael Schlueter <schluet0r4life@gmail.com>
2023-08-28 18:10:42 +0200

    add code syntax highlighting with bright

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 
 import BlogHero from '@/components/BlogHero';
+import CodeSnippet from '@/components/CodeSnippet/CodeSnippet';
 
 import styles from './postSlug.module.css';
 import { loadBlogPost } from '@/helpers/file-helpers';
@@ -27,7 +28,9 @@ async function BlogPost({ params }) {
         publishedOn={frontmatter.publishedOn}
       />
       <div className={styles.page}>
-        <MDXRemote source={content} />
+        <MDXRemote source={content} components={{
+          pre: CodeSnippet,
+        }} />
       </div>
     </article>
   );
