90f03713d28b76f3f9802bde0aa26c6bb5bbfb4f
Michael Schlueter <schluet0r4life@gmail.com>
2023-08-29 12:50:23 +0200

    rendering DivisionGroupsDemo widget in MDX

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { MDXRemote } from 'next-mdx-remote/rsc';
+import React from "react";
+import { MDXRemote } from "next-mdx-remote/rsc";
+import COMPONENT_MAP from "@/helpers/mdx-components";
 
-import BlogHero from '@/components/BlogHero';
-import CodeSnippet from '@/components/CodeSnippet/CodeSnippet';
+import BlogHero from "@/components/BlogHero";
 
-import styles from './postSlug.module.css';
-import { loadBlogPost } from '@/helpers/file-helpers';
-import { BLOG_TITLE } from '@/constants';
+import styles from "./postSlug.module.css";
+import { loadBlogPost } from "@/helpers/file-helpers";
+import { BLOG_TITLE } from "@/constants";
 
 export async function generateMetadata({ params }) {
   const { frontmatter } = await loadBlogPost(params.postSlug);
@@ -14,11 +14,10 @@ export async function generateMetadata({ params }) {
   return {
     title: `${frontmatter.title} • ${BLOG_TITLE}`,
     description: frontmatter.abstract,
-  }
+  };
 }
 
 async function BlogPost({ params }) {
-
   const { frontmatter, content } = await loadBlogPost(params.postSlug);
 
   return (
@@ -28,9 +27,7 @@ async function BlogPost({ params }) {
         publishedOn={frontmatter.publishedOn}
       />
       <div className={styles.page}>
-        <MDXRemote source={content} components={{
-          pre: CodeSnippet,
-        }} />
+        <MDXRemote source={content} components={COMPONENT_MAP} />
       </div>
     </article>
   );
--- src/components/DivisionGroupsDemo/index.js
+++ src/components/DivisionGroupsDemo/index.js
@@ -1,2 +1,5 @@
-export * from './DivisionGroupsDemo';
-export { default } from './DivisionGroupsDemo';
+import dynamic from "next/dynamic";
+
+const DivisionGroupsDemo = dynamic(() => import("./DivisionGroupsDemo"));
+
+export default DivisionGroupsDemo;
